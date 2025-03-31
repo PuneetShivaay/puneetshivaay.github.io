7da805afb37b4160077194e34a6b91072a3f4a8d
Puneet Kumar <Puneetshivaay@gmail.com>
2025-03-31 12:38:27 +0530

    Update Navbar.jsx
    Logo location updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import logo from "../assets/puneetLogo.png";
+import logo from "../assets/PuneetLogo.png";
 import { NAVIGATION_LINKS } from "../constants";
 import { FaTimes } from "react-icons/fa";
 import { FaBars } from "react-icons/fa6";
